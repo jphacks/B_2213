a4b39d2188e77d3f647bbff3a0e33674001bf618
Hasegawa-Akito <h.akito1999@gmail.com>
2022-09-19 00:26:11 +0900

    room_id入力欄追加

---
+++ frontend/src/components/atoms/form/start/RoomIdInput.tsx
@@ -0,0 +1,20 @@
+const RoomIdInput = () => {
+  return (
+    <div className="mt-6">
+      <h1 className="text-3xl sm:text-4xl capitalize tracking-widest lg:text-4xl">
+        Room ID
+      </h1>
+
+      <div className="mt-3">
+        <input
+          id="roomid"
+          type="text"
+          className="block w-full px-4 py-2 mt-2 bg-poker-color rounded-md border-gold focus:outline-none"
+          autoComplete="off"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RoomIdInput;
--- frontend/src/components/modules/forms/start/JoinRoomForm.tsx
+++ frontend/src/components/modules/forms/start/JoinRoomForm.tsx
@@ -1,11 +1,12 @@
 import UserNameInput from "../../../atoms/form/start/UserNameInput";
 import SendButton from "../../../atoms/form/start/SendButton";
+import RoomIdInput from "../../../atoms/form/start/RoomIdInput";
 
 const JoinRoomForm = () => {
   return (
     <div className="max-w-2xl text-center">
       <UserNameInput />
-
+      <RoomIdInput />
       <SendButton />
     </div>
   );
