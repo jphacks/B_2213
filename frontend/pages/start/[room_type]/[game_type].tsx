f35b277c94baa3d40892497d0ad9a8d96508e482
Hasegawa-Akito <h.akito1999@gmail.com>
2022-09-19 18:27:58 +0900

    十文字以下の入力規制

--- frontend/pages/start/[room_type]/[game_type].tsx
+++ frontend/pages/start/[room_type]/[game_type].tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import BackStartPage from "../../../src/components/atoms/transition/start/BackStartPage";
 import CreateRoomForm from "../../../src/components/modules/forms/start/CreateRoomForm";
 import JoinRoomForm from "../../../src/components/modules/forms/start/JoinRoomForm";
 
@@ -8,11 +9,20 @@ const JoinRoom: NextPage = () => {
   const router = useRouter();
   const { game_type, room_type } = router.query;
 
+  // 指定の動的ルーティング以外はstartページへリダイレクト
+  if (
+    (game_type !== "poker" && game_type !== "mahjong") ||
+    (room_type !== "create_room" && room_type !== "join_room")
+  ) {
+    router.push("/start");
+  }
+
   return (
     <div className="bg-poker-color font-poker-color font-poker-family">
       <section className="h-screen bg-cover">
         <div className="flex h-full w-full items-center justify-center container mx-auto px-8">
-          {room_type == "create_room" ? (
+          <BackStartPage game_type={game_type as string} />
+          {room_type === "create_room" ? (
             <div>
               <CreateRoomForm />
             </div>
--- frontend/src/components/atoms/form/start/UserNameInput.tsx
+++ frontend/src/components/atoms/form/start/UserNameInput.tsx
@@ -8,6 +8,11 @@ type UserStateType = {
 // メモ化により不必要な再描画をなくす
 // eslint-disable-next-line react/display-name
 const UserNameInput = memo<UserStateType>(({ user_name, setUserName }) => {
+  // 十文字制限
+  const changeUserName = (changeValue: string) => {
+    if (changeValue.length <= 10) setUserName(changeValue);
+  };
+
   return (
     <div>
       <h1 className="text-3xl sm:text-4xl capitalize tracking-widest lg:text-4xl">
@@ -19,7 +24,7 @@ const UserNameInput = memo<UserStateType>(({ user_name, setUserName }) => {
           id="username"
           type="text"
           value={user_name}
-          onChange={(e) => setUserName(e.target.value)}
+          onChange={(e) => changeUserName(e.target.value)}
           className="block w-full px-4 py-2 mt-2 bg-poker-color rounded-md border-gold focus:outline-none"
           autoComplete="off"
         />
