5845cb45d476a96581a90836bb8e5b766e7f7cda
Hasegawa-Akito <h.akito1999@gmail.com>
2022-09-21 00:43:26 +0900

    routerによるバグ修正

--- frontend/pages/start/[room_type]/[game_type].tsx
+++ frontend/pages/start/[room_type]/[game_type].tsx
@@ -3,10 +3,21 @@ import { useRouter } from "next/router";
 import BackStartPage from "../../../src/components/atoms/transition/start/BackStartPage";
 import CreateRoomForm from "../../../src/components/modules/forms/start/CreateRoomForm";
 import JoinRoomForm from "../../../src/components/modules/forms/start/JoinRoomForm";
+import Loading from "../../../src/components/templates/Loading";
 
 const JoinRoom: NextPage = () => {
   // 動的ルーティングの各値を取得
   const router = useRouter();
+  const isReady = router.isReady;
+
+  // 初回ロード時は最初queryの値を取ってこれずqueryはundifindとなる
+  // またその時にpushメソッドを呼んでも準備ができておらずpush実行時エラーとなる
+  // 以下によりrouterの準備ができるまでLoadingで回避
+  // SSGを用いるときはrouter.isReadyはuseEffect の中で使わなければならないことに注意
+  if (!isReady) {
+    return <Loading />;
+  }
+
   const { game_type, room_type } = router.query;
 
   // 指定の動的ルーティング以外はstartページへリダイレクト
@@ -14,9 +25,6 @@ const JoinRoom: NextPage = () => {
     (game_type !== "poker" && game_type !== "mahjong") ||
     (room_type !== "create_room" && room_type !== "join_room")
   ) {
-    // 初回ロード時は最初queryの値を取ってこれずqueryはundifindとなる
-    // またその時にpushメソッドを呼んでも準備ができておらず以下を実行できないためエラーとなる
-    // isReadyでバグ修正必須
     router.push("/start");
   }
 
