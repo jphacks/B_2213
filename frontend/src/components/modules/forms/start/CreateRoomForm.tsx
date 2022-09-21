65c53ef0c5f099041ec96fbead98e0318c638e93
Hasegawa-Akito <h.akito1999@gmail.com>
2022-09-21 18:12:15 +0900

    エラーコンポーネント作成

--- frontend/src/components/modules/forms/start/CreateRoomForm.tsx
+++ frontend/src/components/modules/forms/start/CreateRoomForm.tsx
@@ -1,14 +1,17 @@
 import UserNameInput from "../../../atoms/form/start/UserNameInput";
 import SendButton from "../../../atoms/form/start/SendButton";
 import { useState } from "react";
+import ErrorMessage from "../../../atoms/form/start/ErrorMessage";
 
 const CreateRoomForm = () => {
   const [user_name, setUserName] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
   const handleSendButton = () => {
     if (user_name.replace(/\s+/g, "")) {
       console.log(user_name);
     } else {
-      console.log("空欄です");
+      setErrorMessage("please input username");
     }
   };
 
@@ -16,6 +19,7 @@ const CreateRoomForm = () => {
     <div className="max-w-2xl text-center">
       <UserNameInput {...{ user_name, setUserName }} />
       <SendButton handleSendButton={() => handleSendButton()} />
+      <ErrorMessage errorMessage={errorMessage} />
     </div>
   );
 };
