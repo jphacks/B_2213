fadc789862b623d257272cb8fb16dd6ae395a192
Hasegawa-Akito <h.akito1999@gmail.com>
2022-10-03 14:36:17 +0900

    id表示とそのコピー機能追加

---
+++ frontend/src/components/atoms/show/ShowRoomId.tsx
@@ -0,0 +1,18 @@
+const ShowRoomId = () => {
+  return (
+    <div className="py-6 capitalize tracking-widest flex flex-row">
+      <button
+        className="px-2 py-1 border-gold-button transition-colors duration-300 transform rounded-md"
+        onClick={() => navigator.clipboard.writeText("aaaaaaaa")} // roomIDをコピー
+        // ^^^^^^ 今後当たられたidをコピーするようする
+      >
+        copy
+      </button>
+      <h1 className="flex justify-center items-center ml-5 text-2xl sm:text-3xl">
+        id : aaaaaaaa
+      </h1>
+    </div>
+  );
+};
+
+export default ShowRoomId;
