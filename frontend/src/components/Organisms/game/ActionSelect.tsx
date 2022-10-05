fd8549f48a211e36bd4a88e7a20796939ae5adaa
Hasegawa-Akito <h.akito1999@gmail.com>
2022-10-06 00:45:07 +0900

    rangeのinputからボタン式に変更

--- frontend/src/components/Organisms/game/ActionSelect.tsx
+++ frontend/src/components/Organisms/game/ActionSelect.tsx
@@ -8,18 +8,24 @@ type ShowActionProps = {
   setShowAction: (showAction: Boolean) => void;
 };
 
+const maxBet = 100; // ゲームでの最高bet額を入れるようにする。
+const allChips = 100001; // 所持しているchipを入れるようにする。
+
 // eslint-disable-next-line react/display-name
 const ActionSelect = memo<ShowActionProps>((props) => {
   const { showAction, setShowAction } = props;
 
+  const [bet, setBet] = useState<number>(maxBet < allChips ? maxBet : allChips); // 初期値はそのゲームでの最高bet額を入れるようにする。
+
   return (
     <div className={showAction ? styles.fadein : styles.fadeout}>
       <div className="bg-[#393939] rounded-t-lg text-center h-full pt-2">
+        {/* maxbetやallchipsはuseContextで渡す */}
         <ActionButtons />
 
-        <BetChips />
+        <BetChips {...{ bet, setBet }} />
 
-        <div className="mt-8">
+        <div className="mt-10">
           <button
             className="px-6 py-2 mr-10 border-gold-button transition-colors duration-300 transform rounded-md"
             onClick={() => setShowAction(false)}
--- frontend/src/components/modules/forms/game/BetChips.tsx
+++ frontend/src/components/modules/forms/game/BetChips.tsx
@@ -1,34 +1,51 @@
 import { memo, useState } from "react";
+import type { BetProps } from "../../../../types/game/type";
+
+const maxBet = 100; // ゲームでの最高bet額を入れるようにする。
+const allChips = 100001; // 所持しているchipを入れるようにする。
 
 // eslint-disable-next-line react/display-name
-const BetChips = memo(() => {
-  const [focusStyle, setFocusStyle] = useState<string>(
-    " border-2 border-[#393939]"
-  );
-  // ^^^初期値を背景色と同じでborderを説得しておくことで
-  // focus時にborderが現れたことでUIが少しズレるのを防ぐ
+const BetChips = memo<BetProps>(({ bet, setBet }) => {
+  const changeBetValue = (betValue: number) => {
+    if (betValue < maxBet) {
+      return setBet(maxBet);
+    }
+    if (betValue > allChips) {
+      return setBet(allChips);
+    }
+
+    return setBet(betValue);
+  };
+
   return (
     <div>
-      <div className="text-6xl py-5 sm:text-5xl lg:text-7xl flex justify-center items-center">
-        <h1 className={"p-2 rounded-md" + focusStyle}>2400000</h1>
+      <div className="pt-5 flex justify-center items-center">
+        <input
+          value={bet}
+          onChange={(e) => setBet(Number(e.target.value))}
+          onBlur={() => changeBetValue(bet)}
+          className="text-center text-5xl p-2 h-16 w-9/12 sm:w-5/12 md:w-3/12 bg-[#4f4e4e] text-[#95913f] rounded-md outline-none"
+        />
       </div>
+      <div className="pt-3">
+        {[2, 3, 4, 5].map((value, key) => {
+          return (
+            <button
+              key={key}
+              className="px-2 py-2 mr-3 border-gold-button transition-colors duration-300 transform rounded-md"
+              onClick={() => changeBetValue(maxBet * value)}
+            >
+              ×{value}
+            </button>
+          );
+        })}
 
-      <div className="tracking-widest flex flex-row justify-center items-center">
-        <input
-          id="range"
-          type="range"
-          className="flex w-10/12 mr-2 accent-[#95913f]"
-          onMouseOver={() => setFocusStyle(" border-2 border-[#95913f]")} // パソコン用
-          onMouseLeave={() => setFocusStyle(" border-2 border-[#393939]")} // パソコン用
-          onTouchMove={() => setFocusStyle(" border-2 border-[#95913f]")} // スマホ・タブレット用
-          onTouchEnd={() => setFocusStyle(" border-2 border-[#393939]")} // スマホ・タブレット用
-        />
-        <input
-          type="number"
-          className="w-6 h-6 rounded-md outline-none bg-[#757575] text-[#757575]"
-          onFocus={() => setFocusStyle(" border-2 border-[#95913f]")}
-          onBlur={() => setFocusStyle(" border-2 border-[#393939]")}
-        />
+        <button
+          className="px-2 py-2 ml-3 border-gold-button transition-colors duration-300 transform rounded-md"
+          onClick={() => changeBetValue(allChips)}
+        >
+          all in
+        </button>
       </div>
     </div>
   );
