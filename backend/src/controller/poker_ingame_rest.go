653bb3e66474b4821cdd852893d2040653ac4967
Kyosuke Fujita <kurofuku01@gmail.com>
2022-10-10 21:14:30 +0900

    StartGameを実装

--- backend/src/controller/poker_ingame_rest.go
+++ backend/src/controller/poker_ingame_rest.go
@@ -16,3 +16,30 @@ func IngameReload(c *gin.Context) {
 	view.NoContext(c)
 	pr[roomID].WritePokerRoomtoWS()
 }
+
+// HnadlerFunc for GET /api/ingame/:roomID/StartGame
+func IngameStartGame(c *gin.Context) {
+	rid := c.Param("roomID")
+	if _, ok := pr[rid]; !ok {
+		view.RequestError(c, "RoomID is Wrong")
+		return
+	}
+
+	room := (pr[rid])
+	uid := c.Query("userID")
+	if uid == "" {
+		view.RequestUnauthorized(c, "UserID in QueryParam is required")
+		return
+	} else if _, ok := room.Users[uid]; !ok {
+		view.RequestUnauthorized(c, "UserID in QueryParam is invalid")
+		return
+	} else if !room.Users[uid].Admin {
+		view.RequestForbidden(c, "You are not admin")
+		return
+	}
+
+	room.RoomData.Round = 1
+
+	view.NoContext(c)
+	room.WritePokerRoomtoWS()
+}
