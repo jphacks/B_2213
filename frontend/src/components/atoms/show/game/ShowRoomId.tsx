efe6599be444435eda488f38611f2b8fd32ca50b
Hasegawa-Akito <h.akito1999@gmail.com>
2022-10-14 18:29:30 +0900

    コピー機能の代替及びエラー処理

--- frontend/src/components/atoms/show/game/ShowRoomId.tsx
+++ frontend/src/components/atoms/show/game/ShowRoomId.tsx
@@ -1,15 +1,17 @@
 import { memo } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 // eslint-disable-next-line react/display-name
 const ShowRoomId = memo<{ roomID: string }>(({ roomID }) => {
   return (
     <div className="py-6 tracking-widest flex flex-row">
-      <button
-        className="px-2 py-1 border-gold-button transition-colors duration-300 transform rounded-md"
-        onClick={() => navigator.clipboard.writeText(roomID)} // roomIDをコピー
-      >
-        copy
-      </button>
+      {/* navigator.clipboard.writeTextがhttpsでないと使えないため代用 */}
+      <CopyToClipboard text={roomID}>
+        <button className="px-2 py-1 border-gold-button transition-colors duration-300 transform rounded-md">
+          copy
+        </button>
+      </CopyToClipboard>
+
       <h1 className="flex justify-center items-center ml-5 text-2xl sm:text-3xl">
         Id : {roomID}
       </h1>
