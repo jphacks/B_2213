36296dc428567bb0582857e8927f6dd5ed423cef
Hasegawa-Akito <h.akito1999@gmail.com>
2022-10-16 17:09:31 +0900

    quitRoom実装

--- frontend/src/components/Organisms/game/StartQuitRoom.tsx
+++ frontend/src/components/Organisms/game/StartQuitRoom.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useRouter } from "next/router";
 import { memo, useContext, useEffect, useState } from "react";
 import { MemberContext } from "../../../../pages/game/waitRoom/[roomId]";
@@ -25,9 +26,29 @@ const StartQuitRoom = memo<PropType>(({ round, setShowOption }) => {
     }
   }, [round]);
 
+  const quitRoom = async () => {
+    try {
+      const apiUrl =
+        process.env.NEXT_PUBLIC_API_URL +
+        "/api/ingame/" +
+        userInfo.roomID +
+        "/quitGame?userID=" +
+        userInfo.userID;
+      await axios.post(apiUrl);
+
+      router.push("/start");
+      return;
+    } catch (e) {
+      return;
+    }
+  };
+
   return (
     <div className="pt-3 pb-20 w-full z-10 absolute bottom-0 left-0 lg:pb-10 bg-poker-color">
-      <button className="px-6 py-2 mr-2 border-gold-button transition-colors duration-300 transform rounded-md">
+      <button
+        className="px-6 py-2 mr-2 border-gold-button transition-colors duration-300 transform rounded-md"
+        onClick={() => quitRoom()}
+      >
         Quit Room
       </button>
       {showOnlyAdmin ? (
